1de6d77aae3d3574ef7bbd34580561a17ca6d043
Mamadou Oury Diallo <mamadouourydiallo@Mamadous-MacBook-Pro.local>
2025-03-26 10:56:51 +0400

    Add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import React from 'react'
 import { ThemeProvider } from '@/components/providers/theme-provider'
 import { AnimationProvider } from '@/components/providers/animation-provider'
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -25,6 +26,7 @@ export default function RootLayout({
             {children}
           </AnimationProvider>
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   )
